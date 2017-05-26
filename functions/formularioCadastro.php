78032979159860797deaeac11f21aaf7379de0a8
Yuri Alexsander <yuri.alexs@gmail.com>
2017-05-26 17:48:50 -0300

    finish

--- functions/formularioCadastro.php
+++ functions/formularioCadastro.php
@@ -19,8 +19,42 @@
     
     
     
-<div class="formCadastro">
 
+
+
+<div class="formLogin log">
+<p class="pLogin">Login</p>
+                       
+            <form method="POST" action="functions/fazerLogin.php">
+              
+                <label for="login">Login ou Email</label>      
+                <input type="text" name="login" required>
+                <label for="password">Senha</label>      
+                <input type="text" name="password" required>
+                <input type="submit" value="Login" name="entrar" class="botao botaoSubmit botaoLog">
+                
+              
+            </form>
+    
+    <form method="POST">
+    <input type="submit" value="Cadastrar" name="cad" class="botaoCadastrar">
+         
+    </form>
+    
+    
+    
+               </div>
+    
+    
+    
+    <?php 
+        
+    if (isset($_POST['cad'])) {
+    
+    ?>
+    
+    <div class="formCadastro cad">
+        
     <p class="pCadastro">Cadastre-se</p>
     
 <form method="POST" action="index.php?p=cadastro">
@@ -45,22 +79,14 @@
 
 
 </div>
-
-
-<div class="formLogin">
-<p class="pLogin">Já possui cadastro?<br> Entre com sua conta</p>
-                       
-            <form method="POST" action="functions/fazerLogin.php">
-              
-                <label for="login">Login ou Email</label>      
-                <input type="text" name="login" required>
-                <label for="password">Senha</label>      
-                <input type="text" name="password" required>
-                <input type="submit" value="Login" name="entrar" class="botao botaoSubmit">
-                
-              
-            </form>
-               </div>
+    
+    <?php 
+        
+    }
+    
+    ?>
+    
+    
               <?php
                    
                    
